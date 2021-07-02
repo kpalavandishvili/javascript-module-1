37be18ebd498aeb6804325e07ed09db4c17f570b
Keti <k.palavandishvili@yahoo.com>
2021-07-02 22:17:21 +0200

    K-function-parameters => done

--- week-1/Homework/K-functions-parameters/exercise4.js
+++ week-1/Homework/K-functions-parameters/exercise4.js
@@ -1,5 +1,8 @@
 // Declare your function first
+function twoNumbers(a, b) {
+  return a + b;
+}
 
 // Call the function and assign to a variable `sum`
-
+let sum = twoNumbers(13, 124);
 console.log(sum);
